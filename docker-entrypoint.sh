d08fd2ba74600b42bb0218ae35f2f0bc9875985c
Chris Joel <chris@scriptolo.gy>
2022-02-01 21:38:01 -0800

    Minimal entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,6 @@
 
 gnunet-arm -c /opt/peer.conf -s > $HOME/gnunet.log 2>&1
 
-gnunet-config -c /opt/peer.conf -s gns-proxy -o AUTOSTART -V YES
-gnunet-config -c /opt/peer.conf -s rest -o BIND_TO -V "127.0.0.1"
-gnunet-config -c /opt/peer.conf -s rest -o BIND_TO6 -V "::1/128"
+
 
 exec gnunet-arm -c /opt/peer.conf -r --monitor
