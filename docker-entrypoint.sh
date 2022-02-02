06e9a4dbef9922510e79c665e587470f9cc64664
Chris Joel <chris@scriptolo.gy>
2022-02-01 21:13:27 -0800

    Updates

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-gnunet-arm -s > $HOME/gnunet.log 2>&1
-#Listen on all interfaces in docker
-gnunet-config -s rest -o BIND_TO -V "0.0.0.0"
-gnunet-config -s rest -o BIND_TO6 -V "::"
-#Restart
-gnunet-arm -r
-exec bash
+
+gnunet-arm -c /opt/peer.conf -s > $HOME/gnunet.log 2>&1
+
+gnunet-config -c /opt/peer.conf -s gns-proxy -o AUTOSTART -V YES
+gnunet-config -c /opt/peer.conf -s rest -o BIND_TO -V "127.0.0.1"
+gnunet-config -c /opt/peer.conf -s rest -o BIND_TO6 -V "::1/128"
+
+exec gnunet-arm -c /opt/peer.conf -r --monitor
